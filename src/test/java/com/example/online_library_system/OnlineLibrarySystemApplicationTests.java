2581f2a1a2af99f6941b4db150bb7da1e1892330
aawwils <sovetkalidemezan@gmail.com>
2026-01-28 14:19:54 +0500

    Initial Spring Boot project

---
+++ src/test/java/com/example/online_library_system/OnlineLibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_library_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLibrarySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
